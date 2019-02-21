78e5b6c56d461cb7c8f682c2b558f3d46dbdf4d8
allen <16023783d@connect.polyu.hk>
2019-02-21 19:32:17 +0800

    bug fix1

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -16,7 +16,7 @@ exports.calculate = function(req, res) {
 	'multiply': function(a,b) { return a * b },
     'divide': function(a,b) { return a / b },
   };
-
+  // bug fix 1
   // Determine the operation
 
   if (! req.query.operation) {
